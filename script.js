98df015fed52a32bc6572b63f5a015b93695e16e
radduC <36330732+radduC@users.noreply.github.com>
2018-03-01 00:06:12 +0200

    Script updated

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-list = [4, 5, 6];
+let list = [4, 5, 6];
 
-list.forEach(element => {
-  element = element + 2;
-});
+// for (i = 0; i < list.length; i++) {
+//   list[i] = list[i] + 10;
+// }
+
+function modify(x, y) {
+  list[y] += 10;
+}
+
+list.forEach(modify);
+
+console.log(list);
